08f5501fe51b10d62d452255f8c31952e50f6f60
NicoliLuz <azevedonicoligithub@gmail.com>
2024-07-16 08:25:54 -0300

    first commit

---
+++ src/components/Forms.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import './Lista'; /* arrumar */
+
+function myForms ({ adicionar }) {
+    const [add, setAdd] = useState('');
+
+    const addSubmit = (e) => {
+        e.preventDefault();
+        adicionar(add);
+        setAdd('');
+    };
+
+    return (
+        <form onSubmit={addSubmit}>
+            <input type="text" value={add} onChange={(e) => setAdd(e.target.value)}
+            placeholder="Adicionar seu prato favorito"></input>
+            <button type="submit">Adicionar</button>
+        </form>
+    );
+}
+
+export default myForms;
---
+++ src/components/Lista.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import "./Lista"; /* arrumar */
+
+/*function myLista */
